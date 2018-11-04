7b9972da607859f5274e2acddf685ebd8ec15a0e
acfasj <acfasj@gmail.com>
2018-11-04 17:24:43 +0800

    feat: tabs lazy load

--- src/api/topic.js
+++ src/api/topic.js
@@ -19,34 +19,21 @@ function getTopicList(options = {}) {
   })
 }
 
-// 精华
-function getGoodTopicList(options = {}) {
-  options.tab = 'good'
-  return getTopicList(options)
-}
+function getTopic(id, options = {}) {
+  const accesstoken = options.accesstoken
+  const mdrender = options.mdrender ? options.mdrender : true
 
-// 招聘
-function getJobTopicList(options = {}) {
-  options.tab = 'job'
-  return getTopicList(options)
-}
-
-// 问答
-function getAskTopicList(options = {}) {
-  options.tab = 'ask'
-  return getTopicList(options)
-}
-
-// 分享
-function getShareTopicList(options = {}) {
-  options.tab = 'share'
-  return getTopicList(options)
+  return fetch({
+    method: 'GET',
+    url: `/topic/${id}`,
+    data: {
+      accesstoken,
+      mdrender,
+    },
+  })
 }
 
 export {
   getTopicList,
-  getGoodTopicList,
-  getJobTopicList,
-  getAskTopicList,
-  getShareTopicList,
+  getTopic,
 }
--- src/common/helper.js
+++ src/common/helper.js
@@ -1,4 +1,3 @@
-const _ = {}
 const SECOND = 1000
 const HOUR = 3600 * SECOND
 const DAY = 24 * HOUR
@@ -15,15 +14,15 @@ function formatTime(date) {
   const second = date.getSeconds()
 
   return (
-    [year, month, day].map(leftPad).join('-') +
+    [ year, month, day ].map(leftPad).join('-') +
     ' ' +
-    [hour, minute, second].map(leftPad).join(':')
+    [ hour, minute, second ].map(leftPad).join(':')
   )
 }
 
 function leftPad(n) {
   n = n.toString()
-  return n[1] ? n : '0' + n
+  return n[ 1 ] ? n : '0' + n
 }
 
 function formatForHuman(date) {
@@ -44,16 +43,109 @@ function formatForHuman(date) {
   }
 }
 
-_.formatDate = function formatDate(date, human = false) {
+function formatDate(date, human = false) {
   date = date instanceof Date ? date : new Date(date)
   const str = formatTime(date)
   if (human) {
     return {
       str,
-      human: formatForHuman(date)
+      human: formatForHuman(date),
     }
   }
   return str
 }
 
-export default _
+/**
+ * 显示菜单
+ */
+function showMenu() {
+  const prompt = require('@system.prompt')
+  const router = require('@system.router')
+  const appInfo = require('@system.app').getInfo()
+  prompt.showContextMenu({
+    itemList: ['保存桌面', '关于', '取消'],
+    success: function(ret) {
+      switch (ret.index) {
+        case 0:
+          // 保存桌面
+          createShortcut()
+          break
+        case 1:
+          // 关于
+          router.push({
+            uri: '/About',
+            params: {
+              name: appInfo.name,
+              icon: appInfo.icon
+            }
+          })
+          break
+        case 2:
+          // 取消
+          break
+        default:
+          prompt.showToast({
+            message: 'error'
+          })
+      }
+    }
+  })
+}
+
+/**
+ * 创建桌面图标
+ * 注意：使用加载器测试`创建桌面快捷方式`功能时，请先在`系统设置`中打开`应用加载器`的`桌面快捷方式`权限
+ */
+function createShortcut() {
+  const prompt = require('@system.prompt')
+  const shortcut = require('@system.shortcut')
+  shortcut.hasInstalled({
+    success: function(ret) {
+      if (ret) {
+        prompt.showToast({
+          message: '已创建桌面图标'
+        })
+      } else {
+        shortcut.install({
+          success: function() {
+            prompt.showToast({
+              message: '成功创建桌面图标'
+            })
+          },
+          fail: function(errmsg, errcode) {
+            prompt.showToast({
+              message: `${errcode}: ${errmsg}`
+            })
+          }
+        })
+      }
+    }
+  })
+}
+
+/**
+ * 防止访问 undefined.prop 报错
+ * 并且模板语法的表达式只能访问到 vm 和 普通的全局变量
+ * 不能访问用户自定义的全局变量
+ * https://doc.quickapp.cn/tutorial/framework/optimization-skills.html
+ */
+function chain(...args) {
+  let ret
+  if (args.length > 0) {
+    ret = args.shift()
+    let tmp
+    while (ret && args.length > 0) {
+      tmp = args.shift()
+      ret = ret[tmp]
+    }
+  }
+
+  return ret || false
+}
+
+export default {
+  formatDate,
+  chain,
+  showMenu,
+  createShortcut,
+}
